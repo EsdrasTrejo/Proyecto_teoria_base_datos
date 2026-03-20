7190932f7ca796ffb98af28ce33647b4262dfabe
EsdrasTrejo <esdrastrejo48@gmail.com>
2026-03-20 17:34:50 -0600

    CRUD USUARIO
    arreglos a procedimiento eliminar

--- database/procedimientos/crud_usuario.sql
+++ database/procedimientos/crud_usuario.sql
@@ -1,82 +1,89 @@
 delimiter $$
-
 drop procedure if exists sp_insertar_usuario $$
-
 create procedure sp_insertar_usuario( p_nombre varchar(300), p_apellido varchar(300), p_correo varchar(300), p_salario_mensual decimal(12,2),
     p_creado_por varchar(300))
 begin
     if p_nombre is null or trim(p_nombre) = '' then
-        signal sqlstate '45000'
-        set message_text = 'el nombre no puede ser nulo o vacio';
+    signal sqlstate '45000'
+    set message_text = 'el nombre no puede ser nulo o vacio';
     end if;
 
     if p_apellido is null or trim(p_apellido) = '' then
-        signal sqlstate '45000'
-        set message_text = 'el apellido no puede ser nulo o vacio';
+    signal sqlstate '45000'
+    set message_text = 'el apellido no puede ser nulo o vacio';
     end if;
 
     if p_correo is null or trim(p_correo) = '' then
-        signal sqlstate '45000'
-        set message_text = 'el correo no puede ser nulo o vacio';
+    signal sqlstate '45000'
+    set message_text = 'el correo no puede ser nulo o vacio';
     end if;
 
     if p_salario_mensual is null then
-        signal sqlstate '45000'
-        set message_text = 'el salario mensual no puede ser nulo';
+    signal sqlstate '45000'
+    set message_text = 'el salario mensual no puede ser nulo';
     end if;
 
     if p_salario_mensual < 0 then
-        signal sqlstate '45000'
-        set message_text = 'el salario mensual no puede ser negativo';
+    signal sqlstate '45000'
+    set message_text = 'el salario mensual no puede ser negativo';
     end if;
 
     if p_creado_por is null or trim(p_creado_por) = '' then
-        signal sqlstate '45000'
-        set message_text = 'el usuario creador no puede ser nulo o vacio';
+    signal sqlstate '45000'
+    set message_text = 'el usuario creador no puede ser nulo o vacio';
     end if;
 
-    if exists (
-        select 1
-        from usuario
-        where lower(trim(correo)) = lower(trim(p_correo))
-    ) then
-        signal sqlstate '45000'
-        set message_text = 'ya existe un usuario con ese correo';
+    if exists ( select 1 from usuario
+    where lower(trim(correo)) = lower(trim(p_correo)) ) then
+    signal sqlstate '45000'
+    set message_text = 'ya existe un usuario con ese correo';
     end if;
 
-    insert into usuario(
-        nombre,
-        apellido,
-        correo,
-        fecha_registro,
-        salario_mensual,
-        estado,
-        creador_user
-    )
-    values(
-        trim(p_nombre),
-        trim(p_apellido),
-        trim(p_correo),
-        curdate(),
-        p_salario_mensual,
-        1,
-        trim(p_creado_por)
-    );
+    insert into usuario(nombre, apellido,correo, fecha_registro, salario_mensual, estado, creador_user)
+    values(trim(p_nombre), trim(p_apellido), trim(p_correo),curdate(),p_salario_mensual,1, trim(p_creado_por) );
 end $$
-
 delimiter ;
+
 call sp_insertar_usuario('esdras','carranza','esdrastrejo@gmail.com',1000,'admin1');
 
-
-
 delimiter $$
+
 drop procedure if exists sp_eliminar_usuario $$
 
-create procedure sp_eliminar_usuario(in p_id_usuario int )
+create procedure sp_eliminar_usuario( p_id_usuario int, p_modificado_por varchar(300))
 begin 
-	update usuario set estado = 0,modificado_en = current_timestamp where id_usuario = p_id_usuario;
-end $$
+    if p_id_usuario is null or p_id_usuario <= 0 then 
+    signal sqlstate '45000'
+    set message_text = 'el id del usuario es invalido';
+    end if;
 
+    if not exists ( select 1 from usuario
+    where id_usuario = p_id_usuario) then
+    signal sqlstate '45000'
+    set message_text = 'el usuario no existe';
+    end if;
+
+    if exists ( select 1 from usuario
+    where id_usuario = p_id_usuario and estado = 0) then
+    signal sqlstate '45000'
+    set message_text = 'el usuario ya se encuentra inactivo';
+    end if;
+
+    if p_modificado_por is null or trim(p_modificado_por) = '' then
+    signal sqlstate '45000'
+    set message_text = 'el usuario modificador no puede ser nulo o vacio';
+    end if;
+
+    -- opcional: bloquear si tiene presupuestos activos
+    if exists ( select 1 from presupuesto
+    where id_usuario = p_id_usuario and estado = 1 ) then
+    signal sqlstate '45000'
+    set message_text = 'no se puede desactivar el usuario porque tiene presupuestos activos';
+    end if;
+
+    update usuario  set estado = 0, modificado_user = trim(p_modificado_por), modificado_fecha = current_timestamp
+    where id_usuario = p_id_usuario;
+end $$
 delimiter ;
 
 delimiter $$ 
@@ -87,83 +94,68 @@ begin
 	creador_user, modificado_por_user, creado_en, modificado_en from usuario where id_usuario=p_id_usuario;
 end $$ 
 delimiter ;
+
 delimiter $$
-
 drop procedure if exists sp_actualizar_usuario $$
-
 create procedure sp_actualizar_usuario( p_id_usuario int, p_nombre varchar(300), p_apellido varchar(300), p_correo varchar(300), p_salario_mensual decimal(12,2),
  p_estado tinyint, p_modificado_por varchar(300))
 begin
     if p_id_usuario is null or p_id_usuario <= 0 then
-        signal sqlstate '45000'
-        set message_text = 'el id del usuario es invalido';
+    signal sqlstate '45000'
+    set message_text = 'el id del usuario es invalido';
     end if;
 
-    if not exists (
-        select 1
-        from usuario
-        where id_usuario = p_id_usuario
-    ) then
-        signal sqlstate '45000'
-        set message_text = 'el usuario no existe';
+    if not exists ( select 1 from usuario
+    where id_usuario = p_id_usuario ) then
+    signal sqlstate '45000'
+    set message_text = 'el usuario no existe';
     end if;
 
     if p_nombre is null or trim(p_nombre) = '' then
-        signal sqlstate '45000'
-        set message_text = 'el nombre no puede ser nulo o vacio';
+    signal sqlstate '45000'
+    set message_text = 'el nombre no puede ser nulo o vacio';
     end if;
 
     if p_apellido is null or trim(p_apellido) = '' then
-        signal sqlstate '45000'
-        set message_text = 'el apellido no puede ser nulo o vacio';
+    signal sqlstate '45000'
+    set message_text = 'el apellido no puede ser nulo o vacio';
     end if;
 
     if p_correo is null or trim(p_correo) = '' then
-        signal sqlstate '45000'
-        set message_text = 'el correo no puede ser nulo o vacio';
+    signal sqlstate '45000'
+    set message_text = 'el correo no puede ser nulo o vacio';
     end if;
 
     if p_salario_mensual is null then
-        signal sqlstate '45000'
-        set message_text = 'el salario mensual no puede ser nulo';
+    signal sqlstate '45000'
+    set message_text = 'el salario mensual no puede ser nulo';
     end if;
 
     if p_salario_mensual < 0 then
-        signal sqlstate '45000'
-        set message_text = 'el salario mensual no puede ser negativo';
+    signal sqlstate '45000'
+    set message_text = 'el salario mensual no puede ser negativo';
     end if;
 
     if p_estado is null or p_estado not in (0,1) then
-        signal sqlstate '45000'
-        set message_text = 'el estado solo puede ser 0 o 1';
+    signal sqlstate '45000'
+    set message_text = 'el estado solo puede ser 0 o 1';
     end if;
 
     if p_modificado_por is null or trim(p_modificado_por) = '' then
-        signal sqlstate '45000'
-        set message_text = 'el usuario modificador no puede ser nulo o vacio';
+    signal sqlstate '45000'
+    set message_text = 'el usuario modificador no puede ser nulo o vacio';
     end if;
 
-    if exists (
-        select 1
-        from usuario
-        where lower(trim(correo)) = lower(trim(p_correo))
-          and id_usuario <> p_id_usuario
-    ) then
-        signal sqlstate '45000'
-        set message_text = 'ya existe otro usuario con ese correo';
+    if exists ( select 1 from usuario 
+    where lower(trim(correo)) = lower(trim(p_correo)) and id_usuario <> p_id_usuario) then
+    signal sqlstate '45000'
+    set message_text = 'ya existe otro usuario con ese correo';
     end if;
 
-    update usuario
-    set nombre = trim(p_nombre),
-        apellido = trim(p_apellido),
-        correo = trim(p_correo),
-        salario_mensual = p_salario_mensual,
-        estado = p_estado,
-        modificado_user = trim(p_modificado_por),
-        modificado_fecha = current_timestamp
+    update usuario set nombre = trim(p_nombre), apellido = trim(p_apellido), correo = trim(p_correo), salario_mensual = p_salario_mensual, estado = p_estado,
+    modificado_user = trim(p_modificado_por), modificado_fecha = current_timestamp
     where id_usuario = p_id_usuario;
 end $$
-
 delimiter ;
 
 delimiter $$
