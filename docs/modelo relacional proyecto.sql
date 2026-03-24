9a43361e5f707a4f97a2309a61bb4be845d3d67c
EsdrasTrejo <esdrastrejo48@gmail.com>
2026-03-24 16:37:15 -0600

    Actualizacion y correcion MR
    Se edito el Modelo relacional en base algunos cambios que se hicieron conforme se avanzaba en el proyecto

--- docs/modelo relacional proyecto.sql
+++ docs/modelo relacional proyecto.sql
@@ -1,114 +1,120 @@
 CREATE TABLE `usuario` (
-  `id_usuario` int PRIMARY KEY,
-  `nombre` varchar(300),
-  `apellido` varchar(300),
-  `correo` varchar(300),
-  `fecha_registro` date,
-  `salario_mensual` double,
-  `estado` bool,
-  `creado_user` varchar(300),
-  `modificado_user` varchar(300),
-  `creado_fecha` timestamp,
-  `modificado_fecha` timestamp
+  `id_usuario` int PRIMARY KEY AUTO_INCREMENT,
+  `nombre` varchar(300) NOT NULL,
+  `apellido` varchar(300) NOT NULL,
+  `correo` varchar(300) UNIQUE NOT NULL,
+  `fecha_registro` date NOT NULL,
+  `salario_mensual` int NOT NULL,
+  `estado` boolean NOT NULL,
+  `creador_user` varchar(300) NOT NULL,
+  `modificado_por_user` varchar(300),
+  `creado_en` timestamp NOT NULL,
+  `modificado_en` timestamp NOT NULL
 );
 
 CREATE TABLE `presupuesto` (
-  `id_presupuesto` int PRIMARY KEY,
-  `id_usuario` int,
-  `nombre` varchar(300),
-  `year_inicio` int,
-  `mes_inicio` int,
-  `year_fin` int,
-  `mes_fin` int,
-  `total_ingresos` double,
-  `total_gastos` double,
-  `total_ahorro` double,
-  `fecha_hora_creacion` date,
-  `estado` bool,
-  `creado_user` varchar(300),
+  `id_presupuesto` int PRIMARY KEY AUTO_INCREMENT,
+  `id_usuario` int NOT NULL,
+  `nombre` varchar(300) NOT NULL,
+  `year_inicio` int NOT NULL,
+  `mes_inicio` int NOT NULL,
+  `year_fin` int NOT NULL,
+  `mes_fin` int NOT NULL,
+  `total_ingresos` decimal(12,2) NOT NULL,
+  `total_gastos` decimal(12,2) NOT NULL,
+  `total_ahorro` decimal(12,2) NOT NULL,
+  `fecha_hora_creacion` timestamp NOT NULL,
+  `estado` boolean NOT NULL,
+  `creado_user` varchar(300) NOT NULL,
   `modificado_user` varchar(300),
-  `creado_fecha` timestamp,
-  `modificado_fecha` timestamp
+  `creado_fecha` timestamp NOT NULL,
+  `modificado_fecha` timestamp NOT NULL
 );
 
 CREATE TABLE `categoria` (
-  `id_categoria` int PRIMARY KEY,
-  `id_usuario` int,
-  `nombre` varchar(300),
-  `descripcion` varchar(300),
-  `tipo` varchar(300),
-  `creado_user` varchar(300),
+  `id_categoria` int PRIMARY KEY AUTO_INCREMENT,
+  `id_usuario_propetario` int NOT NULL,
+  `nombre` varchar(300) NOT NULL,
+  `descripcion` varchar(1000),
+  `tipo` varchar(300) NOT NULL,
+  `creado_user` varchar(300) NOT NULL,
   `modificado_user` varchar(300),
-  `creado_fecha` timestamp,
-  `modificado_fecha` timestamp
+  `creado_fecha` timestamp NOT NULL,
+  `modificado_fecha` timestamp NOT NULL
 );
 
 CREATE TABLE `subcategoria` (
-  `id_subcategoria` int PRIMARY KEY,
-  `id_categoria` int,
-  `nombre` varchar(300),
+  `id_subcategoria` int PRIMARY KEY AUTO_INCREMENT,
+  `id_categoria` int NOT NULL,
+  `nombre` varchar(300) NOT NULL,
   `descripcion` varchar(300),
-  `indicador_activa` bool,
-  `es_defecto` bool,
-  `creado_user` varchar(300),
+  `indicador_activa` boolean NOT NULL,
+  `es_defecto` boolean NOT NULL,
+  `creado_user` varchar(300) NOT NULL,
   `modificado_user` varchar(300),
-  `creado_fecha` timestamp,
-  `modificado_fecha` timestamp
+  `creado_fecha` timestamp NOT NULL,
+  `modificado_fecha` timestamp NOT NULL
 );
 
 CREATE TABLE `presupuesto_detalle` (
-  `id_presupuesto_detalle` int PRIMARY KEY,
-  `id_presupuesto` int,
-  `id_subcategoria` int,
-  `monto_mensual` double,
-  `observaciones` varchar(500),
-  `creado_user` varchar(300),
+  `id_presupuesto_detalle` int PRIMARY KEY AUTO_INCREMENT,
+  `id_presupuesto` int NOT NULL,
+  `id_subcategoria` int NOT NULL,
+  `monto_mensual` decimal(12,2) NOT NULL,
+  `observaciones` varchar(1000),
+  `creado_user` varchar(300) NOT NULL,
   `modificado_user` varchar(300),
-  `creado_fecha` timestamp,
-  `modificado_fecha` timestamp
+  `creado_fecha` timestamp NOT NULL,
+  `modificado_fecha` timestamp NOT NULL
 );
 
 CREATE TABLE `obligacion_fija` (
-  `id_obligacion` int PRIMARY KEY,
-  `id_usuario` int,
-  `id_subcategoria` int,
-  `nombre` varchar(300),
+  `id_obligacion` int PRIMARY KEY AUTO_INCREMENT,
+  `id_subcategoria` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `nombre` varchar(300) NOT NULL,
   `descripcion` varchar(500),
-  `monto_mensual` double,
-  `dia_vencimiento` int,
-  `vigente` bool,
-  `fecha_inicio` date,
+  `monto_mensual` decimal(12,2) NOT NULL,
+  `dia_vencimiento` int NOT NULL,
+  `vigente` boolean NOT NULL,
+  `fecha_inicio` date NOT NULL,
   `fecha_finalizacion` date,
-  `creado_user` varchar(300),
+  `creado_user` varchar(300) NOT NULL,
   `modificado_user` varchar(300),
-  `creado_fecha` timestamp,
-  `modificado_fecha` timestamp
+  `creado_fecha` timestamp NOT NULL,
+  `modificado_fecha` timestamp NOT NULL
 );
 
 CREATE TABLE `transaccion` (
-  `id_transaccion` int PRIMARY KEY,
-  `id_usuario` int,
-  `id_presupuesto` int,
-  `year` int,
-  `mes` int,
-  `id_subcategoria` int,
+  `id_transaccion` int PRIMARY KEY AUTO_INCREMENT,
+  `id_usuario` int NOT NULL,
+  `id_presupuesto` int NOT NULL,
+  `anio` int NOT NULL,
+  `mes` int NOT NULL,
+  `id_subcategoria` int NOT NULL,
   `id_obligacion` int,
-  `tipo_transaccion` varchar(300),
+  `tipo` varchar(300) NOT NULL,
   `descripcion` varchar(1000),
-  `monto` double,
-  `fecha` date,
+  `monto` decimal(12,2) NOT NULL,
+  `fecha` date NOT NULL,
   `metodo_pago` varchar(300),
   `num_factura` varchar(300),
   `observaciones` varchar(1000),
-  `creado_user` varchar(300),
+  `creado_user` varchar(300) NOT NULL,
   `modificado_user` varchar(300),
-  `creado_fecha` timestamp,
-  `modificado_fecha` timestamp
+  `creado_fecha` timestamp NOT NULL,
+  `modificado_fecha` timestamp NOT NULL
+);
+
+CREATE TABLE `obligacionfija_transaccion` (
+  `id_obligacion` int NOT NULL,
+  `id_transaccion` int NOT NULL,
+  PRIMARY KEY (`id_obligacion`, `id_transaccion`)
 );
 
 ALTER TABLE `presupuesto` ADD FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
 
-ALTER TABLE `categoria` ADD FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+ALTER TABLE `categoria` ADD FOREIGN KEY (`id_usuario_propetario`) REFERENCES `usuario` (`id_usuario`);
 
 ALTER TABLE `subcategoria` ADD FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`);
 
@@ -116,9 +122,9 @@ ALTER TABLE `presupuesto_detalle` ADD FOREIGN KEY (`id_presupuesto`) REFERENCES 
 
 ALTER TABLE `presupuesto_detalle` ADD FOREIGN KEY (`id_subcategoria`) REFERENCES `subcategoria` (`id_subcategoria`);
 
-ALTER TABLE `obligacion_fija` ADD FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
-
 ALTER TABLE `obligacion_fija` ADD FOREIGN KEY (`id_subcategoria`) REFERENCES `subcategoria` (`id_subcategoria`);
+
+ALTER TABLE `obligacion_fija` ADD FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
 
 ALTER TABLE `transaccion` ADD FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
 
@@ -127,3 +133,7 @@ ALTER TABLE `transaccion` ADD FOREIGN KEY (`id_presupuesto`) REFERENCES `presupu
 ALTER TABLE `transaccion` ADD FOREIGN KEY (`id_subcategoria`) REFERENCES `subcategoria` (`id_subcategoria`);
 
 ALTER TABLE `transaccion` ADD FOREIGN KEY (`id_obligacion`) REFERENCES `obligacion_fija` (`id_obligacion`);
+
+ALTER TABLE `obligacionfija_transaccion` ADD FOREIGN KEY (`id_obligacion`) REFERENCES `obligacion_fija` (`id_obligacion`);
+
+ALTER TABLE `obligacionfija_transaccion` ADD FOREIGN KEY (`id_transaccion`) REFERENCES `transaccion` (`id_transaccion`);
